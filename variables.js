707ecac1c45408ec283bdc361e654e539f049874
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-02-07 13:15:57 -0500

    js code

---
+++ variables.js
@@ -0,0 +1,40 @@
+// const accountId = 1443;
+// // accountId = 112;
+
+// var souravAge = 20;
+// var souravAge = 19;
+
+// {
+//   let gouravAge = 24;
+// }
+// let gouravAge = 25;
+
+// console.table([accountId, souravAge, gouravAge]);
+
+// const myfunction1 = () => {
+//   console.log("kidda");
+// };
+
+// // myfunction1();
+
+// function myfunction2() {
+//   console.log("World!");
+// }
+
+// // myfunction2();
+
+// const var1 = null;
+
+// console.log(typeof var1);
+
+// const myStr = "Gagan";
+// console.log(myStr.charAt(2));
+
+// const myStr = "        Gagan    ";
+// console.log(myStr.trim());
+
+// const mystr = "Gurdeep Singh";
+// console.log(mystr.includes("deeps"));
+
+const randomNumber = Math.random() * 100;
+console.log(randomNumber);
