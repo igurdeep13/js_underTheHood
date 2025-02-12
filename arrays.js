6af92016b8ccb58935639e3c4f34a00259c6f073
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-02-11 21:26:06 -0500

    Array codes

---
+++ arrays.js
@@ -0,0 +1,46 @@
+const myArr = ["Gurdeep", "Karandeep", "Lovepreet"];
+// console.log(myArr);
+
+// Array methods
+// myArr.push("Agamveer");
+// console.log(myArr);
+
+// myArr.unshift("Ishpreet");
+// console.log(myArr);
+
+// myArr.shift();
+// console.log(myArr);
+
+// myArr.includes("gurdeep");
+
+// Join() : Adds all the elements into a string specified by specified separater.
+// console.log(myArr.join("-"));
+
+// Slice() : Return a section of the array based on given index range. But, it does not change or manipulate the orignal array.
+// const newArry = [0, 1, 2, 3, 4, 5];
+// console.log("A =>", newArry);
+
+// console.log("B =>", newArry.slice(1, 4));
+// console.log(newArry);
+
+// Splice(): Remove or manipulate the section based on range from the orignal array
+
+// console.log("C =>", newArry.splice(1, 4));
+// console.log(newArry);
+
+// const boys = ["Gurdeep", "Lovepreet", "Karandeep", "Agamveer"];
+// const girls = ["Navneet", "Taranjit", "Ishpreet"];
+
+//SPREAD OPERATOR
+// const allKids = [...boys, ...girls];
+// console.log(allKids);
+
+// FROM METHOD
+// console.log(Array.from("Gurdeep"));
+// console.log(Array.isArray({}));
+
+// OF METHOD
+// const s1 = 1;
+// const s2 = 2;
+// const s3 = 3;
+// console.log(Array.of(s1, s2, s3));
